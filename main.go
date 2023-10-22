d2fe62be160a7ef70919982d430dc1e59a403df2
sebastian-j-ibanez <sebas.ibanez219@gmail.com>
2023-10-22 17:11:49 -0400

    Initial Commit

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	//"container/vector"
 	"net/http"
+	//"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+type SubmissionTemplate struct {
+	Name             string
+	ValidExtension   string
+	InvalidExtension string
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "Index",
 		})
 	})
+
+	r.GET("/template", func(c *gin.Context) {
+		//filename := c.Query("filename")
+		//fileout, err := os.Open(filename + ".json")
+
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
